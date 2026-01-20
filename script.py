c51af85fef43e07dbce15e8202b80161bcbda5ff
Nasif Azam <62034075+Nasif-Azam@users.noreply.github.com>
2026-01-20 15:10:54 +0600

    Update script.py

--- script.py
+++ script.py
@@ -8,9 +8,7 @@ API:
   Fabric Rest API for role assign.
 
 service_principal_creds:
-    tenant_id_env: "ca3f056e-4448-425a-92a9-e9d3291ea2f3"
-    client_id_env: "c8bbc001-4cfc-4041-897d-949857474f4f"
-    client_secret_env: "rVZ8Q~XmrhsEUgX6vC6iZmf.tVsesxp_6sNOkaSW"
+
 
  
 Capacity ID: 

4b01b76a201f7bb39d6a5a51db33efa45b9d3230
Nasif Azam <62034075+Nasif-Azam@users.noreply.github.com>
2026-01-20 15:10:39 +0600

    Create script.py

---
+++ script.py
@@ -0,0 +1,16 @@
+1. Fabric Token.
+2. Workspace Creation if not exist.
+3. Role assigen into WS.
+4. Deploy Fabric items.
+
+API: 
+  Fabric Rest API for WS creation.
+  Fabric Rest API for role assign.
+
+service_principal_creds:
+    tenant_id_env: "ca3f056e-4448-425a-92a9-e9d3291ea2f3"
+    client_id_env: "c8bbc001-4cfc-4041-897d-949857474f4f"
+    client_secret_env: "rVZ8Q~XmrhsEUgX6vC6iZmf.tVsesxp_6sNOkaSW"
+
+ 
+Capacity ID: 
